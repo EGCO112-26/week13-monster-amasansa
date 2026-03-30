3e50b26a9b404c2f2267be80a35063d2f68d9a11
asnasama <asnabbb@gmail.com>
2026-03-30 08:33:00 +0000

    update

--- main.cpp
+++ main.cpp
@@ -6,10 +6,17 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 #include "monster.h"
 
-int main(int argc, const char * argv[]) {
- 
+int main(int argc, const char * argv[]) 
+{
+    monster *p;
+    monster m1("asna",10,1);
+    monster m2("asma",20,5);
+    monster x[5]={{"asda",50,7},{"amna",700,8}};
+    p=new monster("Lulu",10,50); // if array : p=new monster[3]={{"Lulu",10,50}};
+    delete p; //Destructor p
     return 0;
 }
--- monster.h
+++ monster.h
@@ -4,7 +4,7 @@
 //
 //  Created by Mingmanas Sivaraksa on 30/3/2566 BE.
 //
-
+#include <iomanip>
 #ifndef monster_h
 #define monster_h
 class monster{
@@ -13,9 +13,21 @@ private:
     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string="bene", int=10,int=10); //constructors พร้อม set default
+    ~monster(); //destructor
 };
 
+monster::~monster(){
+    cout << "Bye bye (:P) " << setfill(' ') << setw(5) <<name << " hp: "<< hp << endl;
+}
+
+monster::monster(string n,int h,int p){
+    name=n;
+    h>100? hp=100:hp=p;
+    p>20? potion=20:potion=p;
+
+    cout << "Monster " << setfill(' ') << setw(5) << name <<" was created!" << endl;
+}
 
 #endif /* monster_h */
